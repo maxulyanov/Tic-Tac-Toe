0caf15b9e0886ac68dd1f4970ba5479db5d1563d
M-Ulyanov <web.ulyanov@gmail.com>
2014-09-06 22:11:49 +0400

    Version 0.5
    Ширина блока с полем вычисляется автоматически
    Добавлена работа настроек с помощью кнопок
    Старт по кнопке начать игру
    Возможность сыграть еще раз
    Добавлена функция сброса глобальных параметров

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-(function($){
+(function($) {
 
-	$(function(){
+	$(function() {
 		
 
 		// Глобальные переменные
-		var num = 3,
+		var fieldSize,
+			difficultySelect,
 			countRow = 1,
 		 	countColum = 1,
 		 	globalCountR = 0,
@@ -19,21 +20,27 @@
 		// Переменные для быстрого изменения
 		var text_Gamer = 'Ваш ход',
 			text_Ai = 'Думаю...',
-			text_startGame = 'Сыграть еще раз',
+			text_startGame = 'Играть еще раз',
 			speed_AiMove = 600;
 
-		// Генерация поля для игры
-		//$(document).on('click','.startgame', function(event){
+		// Блок с настройками игры
+		var settingsBlock = $('#settings');
 
+		// Генерация поля для игры
+		$(document).on('click','.startgame', function(event) {
 			event.preventDefault();
-			/*
+
 			$('#field').remove();
+			$(settingsBlock).hide();
 			$(this).remove();
-			*/
+
+			//Получение данных для генерации
+			fieldSize = parseInt($('#settings-field-size').find('a.active').attr('data-fieldsize'));
+			difficultySelect = parseInt($('#settings-difficulty').find('a.active').attr('data-difficulty'));
 
 			// Генерация поля для игры
 			var field = $('<div id="field">');
-			for(var i = 1; i <= num * num; i++){
+			for(var i = 1; i <= fieldSize * fieldSize; i++){
 				var el = $('<div class="cell c-'+i+'"><span></span></div>');
 				arrElems.push(el);
 				$(el).appendTo(field);				
@@ -42,10 +49,10 @@
 			// Деление на строки
 			$(arrElems).each(function(index) {
 				index++;
-				var resultInt = isInteger(index / num);		
+				var resultInt = isInteger(index / fieldSize);		
 				if(resultInt){
 					$(this).attr('data-row', countRow);
-					for(var i = 0; i < num; i++){
+					for(var i = 0; i < fieldSize; i++){
 						$(arrElems[--index]).attr('data-row', countRow);
 					}
 					countRow++;
@@ -53,15 +60,15 @@
 			});
 
 			// Деление на колонки
-			for(var i = 0; i < num; i++){
+			for(var i = 0; i < fieldSize; i++){
 				arrColum.push(arrElems[i]);
 			}
 			$(arrColum).each(function(index) {
 				var innerI = globalCountR;
 				$(this).attr('data-colum', countColum)
-				for(var i = 0; i < num; i++){
+				for(var i = 0; i < fieldSize; i++){
 					$(arrElems[innerI]).attr('data-colum', countColum);
-					innerI = innerI + num;
+					innerI = innerI + fieldSize;
 				}
 
 				globalCountR++;
@@ -70,14 +77,17 @@
 			});
 
 			// Добавление поля и текста в DOM
-			$(field).appendTo($('#wrapper'));
-			$('<h3 class="whose-move">' + text_Gamer + '</h3>').appendTo($('#wrapper'));
+			$(field).appendTo($('#wrapper-game'));
+			$('<h3 class="whose-move">' + text_Gamer + '</h3>').appendTo($('#wrapper-game'));
 
-		//});
+			var width = $('.cell').width() * fieldSize;
+			$('#field').css('width', width+10);
+
+		});
 	
 		// Функция проверки на целое число (для корректного деления на строки)
-		function isInteger(num) {
-  			return (num ^ 0) === num;
+		function isInteger(fieldSize) {
+  			return (fieldSize ^ 0) === fieldSize;
 		};
 
 		// Клик игрока по полю
@@ -92,12 +102,17 @@
 				$('.whose-move').text(text_Ai);
 
 				//Запустить анализатор хода для AI
-				if(!win){
+				if(!$(field).hasClass('endgame')){
 					setTimeout(analizator, speed_AiMove);
 				}			
 			}
 		});
 
+		// Переключение активности для кнопок настроек
+		$('#settings-difficulty, #settings-field-size').find('a').on('click', function() {
+			$(this).parent().parent('div').find('a').removeClass('active');
+			$(this).addClass('active');
+		});
 
 		// Проверка на победу
 		function victory(sign) {
@@ -116,7 +131,7 @@
 						var combo;
 						var mark = $(elem).attr('data-mark');
 
-						for(var i = 0; i < num; i++){
+						for(var i = 0; i < fieldSize; i++){
 							if(line == 0){
 								dataColum--;
 							}
@@ -135,14 +150,14 @@
 							combo  = $('div[data-colum ="' + dataColum + '"][data-row ="' + dataRow + '"][data-mark ="' + mark + '"]');
 							if(typeof(combo)  !== 'undefined' && $(combo).hasClass('there')){
 								checkElems.push(combo);
-								if($(checkElems).length == (num - 1)){
+								if($(checkElems).length == (fieldSize - 1)){
 									checkElems.push(elem);
 								}
 							}
 						}
 
-						if($(checkElems).length === num){
-							$(checkElems).each(function(){
+						if($(checkElems).length === fieldSize){
+							$(checkElems).each(function() {
 								$(this).css('background', 'red');
 							})
 							messWin(sign);
@@ -154,7 +169,7 @@
 			});
 
 			// Ничья
-			if($('.cell.there').length == (num * num) && !win){
+			if($('.cell.there').length == (fieldSize * fieldSize) && !win){
 				messWin();
 				return false;
 			}
@@ -167,7 +182,7 @@
 			var mark;
 			for(var i = 0; i < 2; i++){
 				i == 0 ? mark = 'n' : mark = 'x';
-				$('.there[data-mark="' + mark + '"]').each(function(){
+				$('.there[data-mark="' + mark + '"]').each(function() {
 					for(var i = 0; i < 4; i++){
 						if(init2)
 							innerAnalizator(this, i);
@@ -181,7 +196,7 @@
 				var warning;
 
 				outer:
-				for(var i = 0; i < num; i++){
+				for(var i = 0; i < fieldSize; i++){
 					if(line == 0){
 						dataColum--;
 					}
@@ -200,7 +215,7 @@
 					warning  = $('div[data-colum ="' + dataColum + '"][data-row ="' + dataRow + '"][data-mark ="' + mark + '"]');
 					if($(warning).length >= 1 && $(warning).hasClass('there')){
 						checkElems.push(warning);
-						if($(checkElems).length == (num - 2)){
+						if($(checkElems).length == (fieldSize - 2)){
 							checkElems.push($(elem));
 							for(var i=0; i<checkElems.length; i++){
 								var dataRow = $(checkElems[i]).attr('data-row');
@@ -223,7 +238,7 @@
 								else if(line == 2){
 										var r = 1;
 										var c = 1;
-									for(var i = 1; i <= num; i++){
+									for(var i = 1; i <= fieldSize; i++){
 										r = i;
 										c = i;
 										var thisElem = $('div[data-colum ="' + c + '"][data-row ="' + r + '"]').not('.there');
@@ -235,8 +250,8 @@
 								}
 								else if(line == 3){
 										var r = 1;
-										var c = num+1;
-									for(var i = 1; i <= num; i++){
+										var c = fieldSize+1;
+									for(var i = 1; i <= fieldSize; i++){
 										r = i;
 										c--;
 										var thisElem = $('div[data-colum ="' + c + '"][data-row ="' + r + '"]').not('.there');
@@ -260,7 +275,7 @@
 
 		// Рандомный AI
 		function randomAi() {
-			var max = num * num -1;
+			var max = fieldSize * fieldSize -1;
 			var min = 0;
 			var rand = min - 0.5 + Math.random()*(max-min+1)
 			rand = Math.round(rand);
@@ -281,11 +296,8 @@
 
 		// Умный AI
 		function smartAi(elem) {
-
 			var rand = difficultyRand();
-			console.log(rand)
 			if(rand !== 5){
-				console.log('if')
 				$(elem).addClass('there');
 				$(elem).attr('data-mark', 'n');
 				$(elem).find('span').text('O');
@@ -297,27 +309,31 @@
 				victory('n');
 			}
 			else{
-				console.log('else')
 				randomAi();
 				init2 = false;
 				smart = true;						
 			}
 		};
 
+		// Шанс ошибки хода AI
 		function difficultyRand() {
-			var min = 1;
-			var max = 5;
-			var rand = min - 0.5 + Math.random() * (max - min + 1);
+			var min = 1,
+				difficultyMax;
+			if(difficultySelect == false){
+				difficultyMax = 5;
+			}
+			else if(difficultySelect == true){
+				difficultyMax = 4;
+			}
+			var rand = min - 0.5 + Math.random() * (difficultyMax - min + 1);
 			return rand = Math.round(rand);
 		}	
 
 
 		// Сообщение о результах игры
 		function messWin(s) {
-			win = true;
-
 			if(s == 'n'){
-				console.log('победил ПК!');
+				console.log('победил AI!');
 			}
 			else if(s == 'x'){
 				console.log('победил игрок!');
@@ -326,9 +342,29 @@
 				console.log('Ничья!');
 			}
 
+			win = true;
+
 			$('#field').addClass('endgame');
 			$('.whose-move').remove();
-			$('<a href="#" class="startgame">' + text_startGame + '</a>').appendTo('#wrapper');
+			$('<a href="#" class="startgame">' + text_startGame + '</a>').appendTo('#wrapper-game');
+			$(settingsBlock).show();
+			resetData();
+		};
+
+		// Сброс глобальных переменных в начальное состояние
+		function resetData() {
+			fieldSize,
+			difficultySelect,
+			countRow = 1,
+		 	countColum = 1,
+		 	globalCountR = 0,
+			win = false,
+			init = true,
+			smart = false,
+			init2,
+			arrElems = [],
+			checkElems = [],
+			arrColum = [];
 		};
 
 
